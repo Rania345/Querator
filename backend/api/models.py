d21f94be1e8d366fce94418b43ab4aa18dac0239
rania345 <raniatriek@gmail.com>
2024-05-16 07:10:11 +0100

    create database tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,3 +12,37 @@ class Query(models.Model):
     def __str__(self):
         # it will return the title
         return self.title
+
+
+class Order(models.Model):
+    customer_id: models.IntegerField()
+    order_time: models.DateTimeField()
+    class Meta:
+        db_table = 'orders'
+
+
+class OrderDetails(models.Model):
+    order_id: models.IntegerField()
+    product_id: models.IntegerField()
+    quantity: models.IntegerField()
+    class Meta:
+        db_table = 'order_details'
+
+
+class Product(models.Model):
+    product_name: models.CharField(max_length=50)
+    category: models.CharField(max_length=50)
+    unit_price: models.DecimalField(decimal_places=2)
+    Stock: models.IntegerField()
+    class Meta:
+        db_table = 'products'
+
+
+class Customers(models.Model):
+    first_name: models.CharField(max_length=50)
+    last_name: models.CharField(max_length=50)
+    email: models.CharField(max_length=100)
+    phone: models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'customers'
